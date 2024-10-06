a1425ac05cee1d7f196012b10cd3ca88f290c14f
LifeHckr <jarodthereal@gmail.com>
2024-10-05 18:34:31 -0700

    Step 2: rice can now be seen.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
 
+let rice_seen : number = 0;
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Hello World!";
@@ -9,6 +10,14 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+const counter_div = document.createElement("div");
+counter_div.innerHTML = `${rice_seen} rice seen.`;
+app.append(counter_div);
+
 const button = document.createElement("button");
 button.innerHTML = "🎑";
 app.append(button);
+button.addEventListener("click", () => {
+    rice_seen += 1;
+    counter_div.innerHTML = `${rice_seen} rice seen.`;
+})
