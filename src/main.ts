20d145ae3b0928c1f48b8bd8cec97638c048cae3
LifeHckr <jarodthereal@gmail.com>
2024-10-05 18:27:13 -0700

    Step 1 complete Added button

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,6 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "🎑";
